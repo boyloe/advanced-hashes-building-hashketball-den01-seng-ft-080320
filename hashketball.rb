9fdaeff85069ab1836fa3a7dee8c924f0be285b2
Bryan Oyloe <boyloe@gmail.com>
2020-06-24 19:42:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -89,10 +89,10 @@ game_hash = {
         :shoe => 16,
         :points => 12, 
         :rebounds => 4,
-        :assists => 12, 
-        :steals => 4,
-        :blocks => 5,
-        :slam_dunks => 5 
+        :assists => 7, 
+        :steals => 22,
+        :blocks => 15,
+        :slam_dunks => 10 
       },
       
       {
